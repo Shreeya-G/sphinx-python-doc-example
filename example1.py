9081123f7a763fd68d2ea56413d07ad9dd2b7422
Shreeya-G <shreeyaguthikonda29@gmail.com>
2022-01-05 20:43:05 +0530

    adding functions to document

---
+++ example1.py
@@ -0,0 +1,21 @@
+def func1(arg1,arg2):
+    """
+    this function takes two arguments, sets the first equal to the second and returns the new first argument
+
+    Args:
+        arg1 ([type]): some value
+        arg2 ([type]): another value
+
+    Returns:
+        [type]: arg1
+    """
+    arg1=arg2
+    return arg1
+
+def func2():
+    """"function with no arguments return None
+
+    Returns:
+        None: None
+    """
+    return None
